454651adbc46d2c029646d4366b564e9bb286fd2
naman <namankumarnarula@gmail.com>
2021-06-06 21:32:07 +0530

    styling

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,11 +1,14 @@
+import styles from '../styles/search.module.css';
 export default function Search({ searchTerm, setSearchTerm }) {
-
 
     function handlequery(e) {
         const { value } = e.target
         setSearchTerm(value);
     }
-    return <>
-        <input type="text" placeholder='search' onChange={handlequery} value={searchTerm} />
-    </>
+    return <div id={styles.searchBar}>
+
+        <i className="fas fa-search fa-2x"></i>
+        <input type="text" placeholder='Search' onChange={handlequery} value={searchTerm} />
+    </div>
+
 }
--- frontend/src/components/table.js
+++ frontend/src/components/table.js
@@ -1,12 +1,19 @@
+import styles from '../styles/table.module.css';
 export default function Table({ aggregate }) {
-    return <>
-        {
+    return <main >
+        <div className={styles.appLayout} id={styles.headings} >
+            <h1>Location</h1>
+            <h1>Salary</h1>
+        </div>
+        
+        {   
             aggregate.map((country, index) => {
                 const { name, salary } = country
-                return <div key={index}>
-                    <h1>{name}:{salary}</h1>
+                return <div key={index} className={styles.appLayout} id={styles.content}>
+                    <h1>{name}</h1>
+                    <h1>${salary}</h1>
                 </div>
             })
         }
-    </>
+    </main>
 }
